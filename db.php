0c29acaa9a532f04caed2adaf35451bebf3e1b61
Shinnolo <simo.28.09o@gmai.com>
2021-03-02 15:45:07 +0100

    Test db

---
+++ db.php
@@ -0,0 +1,26 @@
+<?php
+
+$ablums = [
+    ['cover'=>'Img\TBDM-Nocturnal.jpg',
+      'name' =>'The Black Dalhia Murder',
+      'album' =>'Nocturnal',
+      'year'=>'2007',
+      'genre'=>'Melodic Death Metal'],
+    ['cover'=>'',
+     'name'=>'',
+     'album'=>'',
+     'year'=>'',
+     'genre'=>'',],
+    ['cover'=>'',
+    'name'=>'',
+    'album'=>'',
+    'year'=>'',
+    'genre'=>'',],
+    ['cover'=>'',
+    'name'=>'',
+    'album'=>'',
+    'year'=>'',
+    'genre'=>'',],
+
+];
+
