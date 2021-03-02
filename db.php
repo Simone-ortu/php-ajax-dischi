b8959037b60fbee92d4d90038ed86b887104aed7
Shinnolo <simo.28.09o@gmai.com>
2021-03-02 17:48:50 +0100

    Inizio milestone 1

--- db.php
+++ db.php
@@ -3,53 +3,47 @@
  $albums= [
      [
       'cover'=>'Img\TBDM-Nocturnal.jpg',
-      'title'=> '',
-      'author' => '',
-      'year' => '',
-      'genre'=> ''
+      'title'=> 'Nocturnal',
+      'author' => 'The Black Dahlia Murder',
+      'year' => '2007',
+      'genre'=> 'Melodic Deathcore'
      ],
      [
-        'cover'=>'',
-        'title'=> '',
-        'author' => '',
-        'year' => '',
-        'genre'=> ''
+        'cover'=>'Img\21savage-WithoutWarning.jpg',
+        'title'=> 'Without Warning',
+        'author' => '21 Savage, Offsett, Metro Boomin',
+        'year' => '2017',
+        'genre'=> 'Trap'
        ],
        [
-        'cover'=>'',
-        'title'=> '',
-        'author' => '',
-        'year' => '',
-        'genre'=> ''
+        'cover'=>'Img\Travis-scott.jpg',
+        'title'=> 'Astroworld',
+        'author' => 'Travis Scott',
+        'year' => '2018',
+        'genre'=> 'Trap'
        ],
        [
-        'cover'=>'',
-        'title'=> '',
-        'author' => '',
-        'year' => '',
-        'genre'=> ''
+        'cover'=>'Img\Future-High-Off-Life.jpg',
+        'title'=> 'High Off Life',
+        'author' => 'Future',
+        'year' => '2020',
+        'genre'=> 'Trap'
        ],
        [
-        'cover'=>'',
-        'title'=> '',
-        'author' => '',
-        'year' => '',
-        'genre'=> ''
+        'cover'=>'Img\NightSkinny.jpg',
+        'title'=> 'Mattoni',
+        'author' => 'Night Skinny',
+        'year' => '2019',
+        'genre'=> 'Trap'
        ],
        [
-        'cover'=>'',
-        'title'=> '',
-        'author' => '',
-        'year' => '',
-        'genre'=> ''
-       ],
-       [
-        'cover'=>'',
-        'title'=> '',
-        'author' => '',
-        'year' => '',
-        'genre'=> ''
+        'cover'=>'Img\Ride-The-Lightning.jpg',
+        'title'=> 'Ride The Lightning',
+        'author' => 'Metallica',
+        'year' => '1984',
+        'genre'=> 'Thrash Metal'
        ]
+    
  ]
 
 
--- index.php
+++ index.php
@@ -1,18 +1,58 @@
-<?php 
+<?php
 include 'db.php'
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Music</title>
+    <link rel="stylesheet" href="dist/app.css">
 </head>
-<body>
 
-<?php foreach($albums as $album){ ?>
- <img src= <?php  echo $album['cover']?> alt="">
-    <?php  } ?>
+<body>
+    <header>
+        <h1>
+            Music
+        </h1>
+    </header>
+
+
+    <main>
+        <div class="card">
+            <?php foreach ($albums as $album) { ?>
+
+                <img src=<?php echo $album['cover'] ?> alt="cover">
+
+                <h2><?php echo $album['title'] ?></h2>
+                <h2><?php echo $album['author'] ?></h2>
+                <h2><?php echo $album['year'] ?></h2>
+                <h2><?php echo $album['genre'] ?></h2>
+            <?php  } ?>
+        </div>
+    </main>
+    <footer>
+
+    </footer>
+
+
 </body>
+
 </html>
+
+
+
+
+
+
+<!-- <?php foreach ($albums as $album) { ?>
+
+    <img src=<?php echo $album['cover'] ?> alt="cover">
+
+    <h2><?php echo $album['title'] ?></h2>
+    <h2><?php echo $album['author'] ?></h2>
+    <h2><?php echo $album['year'] ?></h2>
+    <h2><?php echo $album['genre'] ?></h2>
+<?php  } ?> -->
